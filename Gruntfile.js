6dc5d099480130a63dafebf3bb7ace5f594f224e
Monika Kosek <mika.kosek@gmail.com>
2017-01-24 13:54:03 +0100

    Usunięcie problemu tworzenia zagnieżdzonych katalogów build. Dodanie synch browser.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,44 +1,62 @@
 module.exports = function(grunt) {
 
   // Project configuration.
-  grunt.initConfig({
+grunt.initConfig({
 	sass: {
 		options: {
-	    	sourceMap: true
-	    },
-	    dist: {
-	    	files: {
-	        	'css/style.css': 'scss/style.scss'
-	        }
-	    }
+			sourceMap: true
+		},
+		dist: {
+			files: {
+				'css/style.css': 'scss/style.scss'
+			}
+		}
 	}, 
 	
 	imagemin: {
-    	dynamic: {
-        	files: [{
-	            expand: true,
-	            cwd: 'images/',
-	            src: ['**/*.{png,jpg,gif}'],
-	            dest: 'images/build/'
-	        }]
-	    }
+		dynamic: {
+			files: [{
+				expand: true,
+				cwd: 'images/',
+				src: ['**/*.{png,jpg,gif}', '!build/*{png,jpg,gif}'],
+				dest: 'images/build/'
+			}]
+		}
 	}, 
 	
 	watch: {
-    	scripts: {
-        	files: ['scss/*.scss'],
-        	tasks: ['sass'],
-        	options: {
-            	spawn: false,
-        	},
-    	} 
+		scripts: {
+			files: ['scss/*.scss'],
+			tasks: ['sass']
+		} 
+	},
+	
+	browserSync: {
+		bsFiles: {
+			src : [
+				'css/*.css',
+				'*.html'
+			]
+		},
+		options: {
+		   	watchTask: true,
+			server: {
+				baseDir: "./"
+	   		}
+		}
 	}
   });
+  
   // Load the plugins tasks 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-browser-sync');
   grunt.loadNpmTasks('grunt-contrib-watch');
   
   // Default task(s).
-  grunt.registerTask('default', ["sass", "imagemin", "watch"]);
+  grunt.registerTask('sassTask', ['sass', 'watch']);
+  grunt.registerTask('buildTask', ['sass', 'imagemin']);
+  grunt.registerTask['workTask', ['browserSync', 'watch']];
+  grunt.registerTask['imageTask', ['imagemin']];
+  grunt.registerTask('default', ['sass', 'imagemin', 'browserSync', 'watch']);
 };
